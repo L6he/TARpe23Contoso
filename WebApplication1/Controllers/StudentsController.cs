5cabcbcca2ce69eb91d5023c0907fd03af31dbbc
opilane <opilane@ORDI-402559>
2024-09-05 10:24:04 +0300

    made the "Details" button display student details

--- WebApplication1/Controllers/StudentsController.cs
+++ WebApplication1/Controllers/StudentsController.cs
@@ -113,5 +113,20 @@ namespace WebApplication1.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
     }
 }
