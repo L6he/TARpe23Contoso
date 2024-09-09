bae91c4f306e89b97542b2c1bd85e97a61f3399f
opilane <opilane@HP-CZC3217068>
2024-09-09 15:46:51 +0300

    added Instructor view, instructor index controller method. help me

--- WebApplication1/Controllers/InstructorController.cs
+++ WebApplication1/Controllers/InstructorController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class InstructorController : Controller
+    public class InstructorController : Controller 
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+        public InstructorController(SchoolContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id, int? courseId)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = await _context.Instructors.Include(i => i.OfficeAssignment).Include(i => i.CourseAssignments).ThenInclude(i => i.Course).ThenInclude(i => i.Enrollments).ThenInclude(i => i.Student)
+                .Include(i => i.CourseAssignments).ThenInclude(i => i.Course).AsNoTracking().OrderBy(i => i.LastName).ToListAsync();
+
+            if (id != null)
+            {
+                ViewData["InstructorID"] = id.Value;
+                Instructor instructor = viewModel.Instructors.Where(i => i.ID == id.Value).Single();
+                viewModel.Courses = instructor.CourseAssignments.Select(i => i.Course);
+            }
+            if (courseId != null)
+            {
+                ViewData["CourseID"] = courseId.Value;
+                viewModel.Enrollments = viewModel.Courses.Where(x => x.CourseID == courseId.Value).Single().Enrollments;
+            }
+            return View(viewModel);
         }
     }
 }
--- WebApplication1/Data/SchoolContext.cs
+++ WebApplication1/Data/SchoolContext.cs
@@ -10,12 +10,18 @@ namespace WebApplication1.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignments> CourseAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Instructor>().ToTable("Instructors");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignments");
+            modelBuilder.Entity<CourseAssignments>().ToTable("CourseAssignments");
             /*modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
