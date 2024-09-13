7f07d5fdec52ebee8ec3f65a98c5f0cd302b07e7
opilane <opilane@HP-CZC3217068>
2024-09-13 08:58:40 +0300

    slight changes to instructor index (still non-functioning), made Clone button display Duplicate

--- WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bfdc6e74ac1d11a53f6ed489e7ee9873375c4ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
