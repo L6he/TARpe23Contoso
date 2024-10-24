677e237a1b1b4458f5824491617ae723ba248976
opilane <opilane@ORDI-402559>
2024-10-24 15:45:17 +0300

    removed delinquent stuff from master

--- WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68639fc283d6a0250a86b95a55cd1972f68d0e95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
